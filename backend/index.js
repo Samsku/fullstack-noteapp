5ce5cf1e0f4f98b4f753ffb38de91042d47634c6
Samsku <turunensami54@gmail.com>
2025-10-23 22:02:09 +0300

    deploy config

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,5 @@
-import cors from "cors";
 import express from "express";
 const app = express()
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react({
-            // Enable Fast Refresh
-            jsxRuntime: 'automatic', // Enables the new JSX transform
-            babel: {
-                plugins: ['@babel/plugin-transform-react-jsx'], // Ensure JSX transform is enabled
-            },
-        }),
-    ],
+    plugins: [react()],
     server: {
-        port: 3000, // Match your frontend port
-        open: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3001',
+                changeOrigin: true,
+            },
+        }
     },
-    build: {
-        outDir: 'dist', // Output directory for the production build
-        sourcemap: true, // Enable source maps for debugging
-    },
-});
+})
