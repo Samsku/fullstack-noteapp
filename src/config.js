df014f5259f93bf8848c825664e0fa4fe12896a9
Samsku <turunensami54@gmail.com>
2025-10-23 18:10:53 +0300

    .

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 export const API_BASE_URL = isProduction 
   ? ''  // In production, API is served from the same domain
-  : 'http://localhost:3001';  // In development, use the backend server URL
+  : 'https://fullstack-noteapp-1-ikgm.onrender.com/';  // In development, use the backend server URL
 
 export default {
   API_BASE_URL
