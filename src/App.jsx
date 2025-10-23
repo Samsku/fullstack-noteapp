2b51b910b94e8208b4cce55c05eb297adf894129
Samsku <turunensami54@gmail.com>
2025-10-23 18:00:01 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Note from './components/Note';
 import Footer from './components/Footer';
 import noteService from './services/notes';
